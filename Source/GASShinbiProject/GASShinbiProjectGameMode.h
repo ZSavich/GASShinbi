cf103ea14a4db961c8d269e20743973fcae73cae
UnrealDesk\Xeka <zsavich59@gmail.com>
2022-01-30 18:16:08 +0200

    Initial Commit

---
+++ Source/GASShinbiProject/GASShinbiProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GASShinbiProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGASShinbiProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGASShinbiProjectGameMode();
+};
+
+
+
